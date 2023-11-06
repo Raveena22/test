bc518ac2ccea9774e9e26cdd0a42ab42b27236ce
Raveena <raveena.ailapuram@braneenterprises.com>
2023-11-06 18:13:16 +0530

    First Commit

---
+++ bitwise.py
@@ -0,0 +1,22 @@
+# import math
+# print(~3)
+# print(12&13)
+# print(math.ceil(2.5))
+# print
+i=1
+j=1
+while(i<=4):
+    print("Raveena")
+    while(i<=3):
+        print("Ailapuram")
+        i+=1
+    j=j+1
+
+for i in range(4):
+    for j in range(4-i):
+        print("# ",end="")
+    print()
+# import numpy as np
+# # for i in range(9):
+# my_array=np.array(int(input(),end=""))
+# print(my_array)
